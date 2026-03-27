a7a5d74bf786a3ffd53d51fea0643d7a90e7936d
Kristi <kwerry4@gmail.com>
2026-03-26 17:08:41 -0700

    cdk set up

---
+++ test/team_comp_diff-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TeamCompDiffAws from '../lib/team_comp_diff-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/team_comp_diff-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TeamCompDiffAws.TeamCompDiffAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
