4235b698370645b96653ad04ef8b191bef98b02d
Kristi <kwerry4@gmail.com>
2026-08-23 22:03:52 -0700

    update amplify platform for NextJS

--- lib/client-stack.ts
+++ lib/client-stack.ts
@@ -1,5 +1,5 @@
 // Define domain, certificate, and DNS settings in a single CDK stack file
-import { App, GitHubSourceCodeProvider, RedirectStatus } from "@aws-cdk/aws-amplify-alpha";
+import { App, GitHubSourceCodeProvider, Platform, RedirectStatus } from "@aws-cdk/aws-amplify-alpha";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
@@ -15,6 +15,7 @@ export class ClientTeamCompDiffStack extends cdk.Stack {
 
     if (stageName === "dev") {
       const amplifyApp = new App(this, `TeamCompDiff`, {
+        platform: Platform.WEB_COMPUTE,
         environmentVariables: {
           NEXT_PUBLIC_API_BASE_URL: props.apiUrl ?? "",
           NEXT_PUBLIC_COGNITO_USER_POOL_ID: props.userpoolId ?? "",
@@ -25,17 +26,7 @@ export class ClientTeamCompDiffStack extends cdk.Stack {
           repository: "TeamCompDiff-Client",
           oauthToken: cdk.SecretValue.secretsManager("github-token"),
         }),
-        autoBranchDeletion: true, // Automatically disconnect a branch when you delete a branch from your repository
-      });
-      amplifyApp.addCustomRule({
-        source: "/<*>",
-        target: "/index.html",
-        status: RedirectStatus.NOT_FOUND,
-      });
-      amplifyApp.addCustomRule({
-        source: String.raw`</^[^.]+$|\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json|otf)$)([^.]+$)/>`,
-        target: "/index.html",
-        status: RedirectStatus.REWRITE,
+        autoBranchDeletion: true,
       });
       amplifyApp.addBranch("main"); //TODO change to dev when we have the branch
     }
@@ -43,27 +34,18 @@ export class ClientTeamCompDiffStack extends cdk.Stack {
     if (stageName === "main") {
       //TODO add hosted zone when url purchased
       const amplifyApp = new App(this, `TeamCompDiff`, {
+        platform: Platform.WEB_COMPUTE,
         environmentVariables: {
-          PUBLIC_API_BASE_URL: props.apiUrl ?? "",
-          PUBLIC_COGNITO_USER_POOL_ID: props.userpoolId ?? "",
-          PUBLIC_COGNITO_USER_POOL_CLIENT_ID: props.userpoolClientId ?? "",
+          NEXT_PUBLIC_API_BASE_URL: props.apiUrl ?? "",
+          NEXT_PUBLIC_COGNITO_USER_POOL_ID: props.userpoolId ?? "",
+          NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID: props.userpoolClientId ?? "",
         },
         sourceCodeProvider: new GitHubSourceCodeProvider({
           owner: "KristiWerry",
           repository: "TeamCompDiff-Client",
           oauthToken: cdk.SecretValue.secretsManager("github-token"),
         }),
-        autoBranchDeletion: true, // Automatically disconnect a branch when you delete a branch from your repository
-      });
-      amplifyApp.addCustomRule({
-        source: "/<*>",
-        target: "/index.html",
-        status: RedirectStatus.NOT_FOUND,
-      });
-      amplifyApp.addCustomRule({
-        source: String.raw`</^[^.]+$|\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json|otf)$)([^.]+$)/>`,
-        target: "/index.html",
-        status: RedirectStatus.REWRITE,
+        autoBranchDeletion: true,
       });
       amplifyApp.addBranch("main");
     }
